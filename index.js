b29dafd57b278049b4492678ca24c830f0e1f09d
Sheikhmsip <sheikhmsip@gmail.com>
2023-11-29 23:33:51 +0600

    connect environment file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 // middleware
 app.use(cors());
@@ -13,7 +15,7 @@ app.get("/", (req, res) => {
 
 
 const uri =
-  "mongodb+srv://JRJBD:7GbZpflT6Tgd9zjg@cluster0.vvvwsgj.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vvvwsgj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
